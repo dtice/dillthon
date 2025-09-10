0e1f4338b9f346527028136e0f5267921793bb0e
dill <dillon.tice@gmail.com>
2025-09-09 21:13:15 -0700

    controllable bar

--- main.py
+++ main.py
@@ -1,6 +1,46 @@
 from PyQt6 import QtCore, QtGui, QtWidgets
 from PyQt6.QtCore import Qt
 
+class VisualizerState(QtCore.QObject):
+    """State object for the visualizer to manage its data and notify changes."""
+    state_changed = QtCore.pyqtSignal()
+
+    def __init__(self):
+        super().__init__()
+        self._value = 0
+        self._vmin = 0
+        self._vmax = 100
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, new_value):
+        if new_value != self._value:
+            self._value = new_value
+            self.state_changed.emit()
+
+    @property
+    def vmin(self):
+        return self._vmin
+
+    @vmin.setter
+    def vmin(self, new_vmin):
+        if new_vmin != self._vmin:
+            self._vmin = new_vmin
+            self.state_changed.emit()
+
+    @property
+    def vmax(self):
+        return self._vmax
+
+    @vmax.setter
+    def vmax(self, new_vmax):
+        if new_vmax != self._vmax:
+            self._vmax = new_vmax
+            self.state_changed.emit()
+
 class _BarVisualizer(QtWidgets.QWidget):
     """Custom widget for a stacked bar visualization"""
     def __init__(self, *args, **kwargs):
@@ -9,6 +49,8 @@ class _BarVisualizer(QtWidgets.QWidget):
             QtWidgets.QSizePolicy.Policy.MinimumExpanding,
             QtWidgets.QSizePolicy.Policy.MinimumExpanding
         )
+        self.visualizer_state = VisualizerState()
+
 
     def sizeHint(self):
         return QtCore.QSize(40, 120)
@@ -22,8 +64,18 @@ class _BarVisualizer(QtWidgets.QWidget):
         painter.fillRect(rect, brush)
 
         # Placeholder state
-        vmin, vmax = 1, 10
-        value = 5
+        value = self.visualizer_state.value
+        vmin = self.visualizer_state.vmin
+        vmax = self.visualizer_state.vmax
+        if vmax <= vmin:
+            vmax = vmin + 1
+
+        if value < vmin:
+            value = vmin
+        
+        if value > vmax:
+            value = vmax
+
         padding = 5
         d_height = painter.device().height() - (padding * 2)
         d_width = painter.device().width() - (padding * 2)
@@ -46,6 +98,30 @@ class _BarVisualizer(QtWidgets.QWidget):
     def _trigger_refresh(self):
         self.update()
 
+    def minimumSizeHint(self):
+        return QtCore.QSize(40, 120)
+    
+class VisualizerControls(QtWidgets.QWidget):
+    """Controls for adjusting the visualizer state."""
+    def __init__(self, visualizer_state: VisualizerState, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.visualizer_state = visualizer_state
+        self._setup_layout()
+
+    def _setup_layout(self):
+        layout = QtWidgets.QFormLayout()
+        
+        self.value_slider = QtWidgets.QSlider(Qt.Orientation.Horizontal)
+        self.value_slider.setMinimum(0)
+        self.value_slider.setMaximum(100)
+        self.value_slider.valueChanged.connect(self._on_value_changed)
+        layout.addRow("Value:", self.value_slider)
+
+        self.setLayout(layout)
+
+    def _on_value_changed(self, value):
+        self.visualizer_state.value = value
+
 class MainWindow(QtWidgets.QMainWindow):
     """Main application window for Dills Badass Thingy."""
     def __init__(self):
@@ -54,8 +130,16 @@ class MainWindow(QtWidgets.QMainWindow):
         self.text = QtWidgets.QPlainTextEdit()
         self.setWindowTitle("Dills Badass Thingy")
         self._setup_palette()
+        self._setup_visualizer()
         self._setup_layout()
         self._setup_menu()
+
+    def _setup_visualizer(self):
+        """Set up the visualizer state and connect it to the bar visualizer."""
+        self.state = VisualizerState()
+        self.bar = _BarVisualizer()
+        self.bar.visualizer_state = self.state
+        self.state.state_changed.connect(self.bar._trigger_refresh)
 
     def _setup_palette(self):
         """Set up dark Fusion palette."""
@@ -77,20 +161,17 @@ class MainWindow(QtWidgets.QMainWindow):
 
     def _setup_layout(self):
         """Set up main window layout with a splitter."""
-        left_layout = QtWidgets.QVBoxLayout()
-        left_layout.addWidget(_BarVisualizer())
-        left_widget = QtWidgets.QWidget()
-        left_widget.setLayout(left_layout)
+        central_widget = QtWidgets.QWidget()
+        self.setCentralWidget(central_widget)
 
-        right_layout = QtWidgets.QVBoxLayout()
-        right_layout.addWidget(self.text)
-        right_widget = QtWidgets.QWidget()
-        right_widget.setLayout(right_layout)
-
+        layout = QtWidgets.QVBoxLayout(central_widget)
         splitter = QtWidgets.QSplitter(Qt.Orientation.Horizontal)
-        splitter.addWidget(left_widget)
-        splitter.addWidget(right_widget)
-        self.setCentralWidget(splitter)
+
+        self.visControls = VisualizerControls(self.state)
+        splitter.addWidget(self.bar)
+        splitter.addWidget(self.visControls)
+
+        layout.addWidget(splitter)
 
     def _setup_menu(self):
         """Set up menu bar and actions."""
@@ -100,23 +181,9 @@ class MainWindow(QtWidgets.QMainWindow):
         open_action.setShortcut(QtGui.QKeySequence.StandardKey.Open)
         menu.addAction(open_action)
 
-        save_action = QtGui.QAction("&Save", self)
-        save_action.triggered.connect(self.save)
-        save_action.setShortcut(QtGui.QKeySequence.StandardKey.Save)
-        menu.addAction(save_action)
-
-        save_as_action = QtGui.QAction("Save &As...", self)
-        save_as_action.triggered.connect(self.save_as)
-        menu.addAction(save_as_action)
-
         close_action = QtGui.QAction("&Close", self)
         close_action.triggered.connect(self.close)
         menu.addAction(close_action)
-
-        help_menu = self.menuBar().addMenu("&Help")
-        about_action = QtGui.QAction("&About", self)
-        about_action.triggered.connect(self.show_about_dialog)
-        help_menu.addAction(about_action)
 
     def open_file(self):
         """Open a file and load its contents into the text editor."""
@@ -125,51 +192,6 @@ class MainWindow(QtWidgets.QMainWindow):
             with open(path) as f:
                 self.text.setPlainText(f.read())
             self.file_path = path
-
-    def save(self):
-        """Save the contents of the text editor to file."""
-        if self.file_path is None:
-            self.save_as()
-        else:
-            with open(self.file_path, "w") as f:
-                f.write(self.text.toPlainText())
-            self.text.document().setModified(False)
-
-    def save_as(self):
-        """Save the contents to a new file."""
-        path = QtWidgets.QFileDialog.getSaveFileName(self, "Save As")[0]
-        if path:
-            self.file_path = path
-            self.save()
-
-    def show_about_dialog(self):
-        """Show the About dialog."""
-        text = (
-            "<center>"
-            "<h1>Text Editor</h1>"
-            "&#8291;"
-            "<img src=icon.svg>"
-            "</center>"
-            "<p>Version 31.4.159.265358<br/>"
-            "Copyright &copy; Company Inc.</p>"
-        )
-        QtWidgets.QMessageBox.about(self, "About Text Editor", text)
-
-    def closeEvent(self, a0):
-        """Prompt to save if there are unsaved changes."""
-        if not self.text.document().isModified():
-            return
-        answer = QtWidgets.QMessageBox.question(
-            self, None,
-            "You have unsaved changes. Save before closing?",
-            QtWidgets.QMessageBox.StandardButton.Save |
-            QtWidgets.QMessageBox.StandardButton.Discard |
-            QtWidgets.QMessageBox.StandardButton.Cancel
-        )
-        if answer & QtWidgets.QMessageBox.StandardButton.Save:
-            self.save()
-        elif answer & QtWidgets.QMessageBox.StandardButton.Cancel:
-            a0.ignore()
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
